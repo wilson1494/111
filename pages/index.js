32cfa9ed880b5440547d8c877b678ac438ab9e3c
wilson1494 <wilson1494@gmail.com>
2021-05-19 20:41:41 +0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Layout from '../components/layout/Layout'
+import MenuContainer from '../components/home/MenuContainer'
+
+export  async function getServerSideProps() {
+  const response = await fetch('https://dry-wave-60207.herokuapp.com/api/categories');
+  const categories = await response.json();
+  return {
+    props:{
+      categories
+    }
+  }
+}
+
+export default function Home({categories}){
+  return(
+    <Layout>
+      <MenuContainer key={categories[0].cid} categories={categories}/>
+    </Layout>
+  )
+}
